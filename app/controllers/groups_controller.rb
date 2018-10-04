d767cfa8bcceb36f7890ec035f84a29227d5941e
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-04 13:16:12 +0900

    Groupsコントローラに必要なアクションを定義 対応するルーティング記述 対応するビューファイル作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,18 @@
 class GroupsController < ApplicationController
+  def new
+    # グループ新規作成画面
+  end
+
+  def create
+    # グループをDBに保存
+  end
+
+  def edit
+    # グループ編集画面
+  end
+
+  def update
+    # グループ編集をDBに反映
+  end
+
 end
