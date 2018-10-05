df529792c976cb2d1f2c0387494a2ad13d5ad958
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-05 21:11:51 +0900

    グループ作成機能を実装 エラーメッセージを日本語化 サイドバーにログイン中のユーザーが参加しているグループを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :groups, through: :members
+  has_many :groups,    through: :members
   has_many :members
+  # has_many :members, dependent: :destroy
 end
