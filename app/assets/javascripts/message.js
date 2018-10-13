eddd8117a4b38c2069147262d05a307c9ba123da
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-13 19:43:28 +0900

    Ajaxメソッドとオプションを記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,5 +4,20 @@ $(() => {
     let formData = new FormData(this);
     let url = window.location.href;
     console.log(url)
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function() {
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    });
   });
 });
