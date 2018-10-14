e45421d1d60f6a8e1c787efbdc105be420cf546a
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-14 17:55:28 +0900

    キー入力時に非同期でユーザー検索を実装 値がjsonで戻ってくることを確認

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,21 @@
 $(() => {
   $('#user-search-field').on('input', function(e) {
     e.preventDefault();
-    console.log("hello")
+    let input = $('#user-search-field').val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input },
+    })
+    .done(function(data) {
+      console.log(data)
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    });
+
   });
 });
