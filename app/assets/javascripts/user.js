dbf4b357f6aa3e8d819e5df46f790404d5536d67
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-15 12:14:16 +0900

    formで送るinputの内容がidではなくnameになっていたため修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -19,7 +19,7 @@ $(() => {
 
   function buildAddUserHTML (name, id) {
     let html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${name}'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
                   <p class='chat-group-user__name'>${name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
@@ -63,4 +63,6 @@ $(() => {
     let html = buildAddUserHTML(userName, userId);
     $('.js-add-user').append(html);
   })
+
+
 });
