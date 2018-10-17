ef45a944f03a01d048f430bfc00e3ff7a3575e1a
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-17 13:13:56 +0900

    Githubのパスの書き方間違いを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com/KazYam1001/chat-space.git"
+set :repo_url, "git@github.com:KazYam1001/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
