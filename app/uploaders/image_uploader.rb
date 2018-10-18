0135e9479e888afb42b7ecb11cd0fb8ffb35378e
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-18 13:24:41 +0900

    fog gemを導入 Awsのキーをプッシュしない設定と Githubを介さず本番環境にはアップロードする記述を追加 Capistranoでキーの入った環境変数を使う記述を追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  # ファイルを800x800に変換する
   process resize_to_fit: [200, 200]
-  storage :file
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
